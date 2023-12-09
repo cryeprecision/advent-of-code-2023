899f4d5c7a64637f3b9eb33b9a11e4148a7be5df
cryeprecision <muench.rico@gmail.com>
2023-12-09 14:09:34 +0100

    optimize parsing

--- src/bin/day-09-part-2.rs
+++ src/bin/day-09-part-2.rs
@@ -1,19 +1,21 @@
 fn main() {
     let mut challenge = advent_of_code_2023::Challenge::start(9, 2);
 
-    let lists = challenge
-        .input_lines()
-        .map(|list| {
-            list.split_whitespace()
-                .map(|num| num.parse::<i64>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+    // put all lines into a single vector to benefit from amortized insertion
+    let mut numbers = Vec::new();
+    let mut line_ends = vec![0];
+
+    challenge.input_lines().for_each(|line| {
+        line.split_whitespace()
+            .map(|num| num.parse::<i64>().unwrap())
+            .for_each(|num| numbers.push(num));
+        line_ends.push(numbers.len());
+    });
     challenge.finish_parsing();
 
-    let solution = lists
-        .into_iter()
-        .map(|mut list| {
+    let solution = line_ends
+        .windows(2)
+        .map(|limits| {
             fn recurse(depth: usize, buffer: &mut [i64]) -> i64 {
                 // check for end of recursion
                 if buffer.len() < 2 || buffer.iter().all(|&num| num == 0) {
@@ -26,10 +28,10 @@ fn main() {
                         buffer[buffer.len() - i - 1] - buffer[buffer.len() - i - 2];
                 }
 
-                let my_result = buffer[0];
-                my_result - recurse(depth + 1, &mut buffer[1..])
+                buffer[0] - recurse(depth + 1, &mut buffer[1..])
             }
 
+            let list = &mut numbers[limits[0]..limits[1]];
             recurse(0, &mut list[..])
         })
         .sum::<i64>();
