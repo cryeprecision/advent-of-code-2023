8fe7ddc0a2eaf034e4b98cf7bda59f1818b79ea5
cryeprecision <muench.rico@gmail.com>
2023-12-15 10:05:30 +0100

    optimize day-10-part-2

--- src/bin/day-10-part-2.rs
+++ src/bin/day-10-part-2.rs
@@ -1,32 +1,46 @@
 #![feature(slice_group_by)]
+#![feature(array_windows)]
 
-use smallvec::{smallvec, SmallVec};
+use std::fmt::Write;
 
-#[allow(dead_code)]
-fn debug_maze(maze: &[Vec<u8>]) -> String {
-    maze.iter()
-        .map(|line| {
-            line.iter()
-                .map(|&c| match c {
+use smallvec::SmallVec;
+
+struct Maze {
+    data: Vec<u8>,
+    width: usize,
+}
+
+impl Maze {
+    fn height(&self) -> usize {
+        self.data.len() / self.width
+    }
+    fn row(&self, row_idx: usize) -> &[u8] {
+        &self.data[(row_idx * self.width)..((row_idx + 1) * self.width)]
+    }
+    fn row_mut(&mut self, row_idx: usize) -> &mut [u8] {
+        &mut self.data[(row_idx * self.width)..((row_idx + 1) * self.width)]
+    }
+}
+
+impl std::fmt::Display for Maze {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        (0..self.height()).for_each(|row_idx| {
+            self.row(row_idx).iter().for_each(|&b| {
+                f.write_char(match b {
                     b'|' => '┃', // is a vertical pipe connecting north and south.
                     b'-' => '━', // is a horizontal pipe connecting east and west.
                     b'L' => '┗', // is a 90-degree bend connecting north and east.
                     b'J' => '┛', // is a 90-degree bend connecting north and west.
                     b'7' => '┓', // is a 90-degree bend connecting south and west.
                     b'F' => '┏', // is a 90-degree bend connecting south and east.
-                    b'.' => '.', // is ground; there is no pipe in this tile.
-                    b'S' => 'S', // is the starting position of the animal;
-                    // there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-                    _ => c as char,
+                    _ => b as char,
                 })
-                .collect::<String>()
-        })
-        .reduce(|mut acc, next| {
-            acc.push('\n');
-            acc.push_str(&next);
-            acc
-        })
-        .unwrap()
+                .unwrap()
+            });
+            f.write_char('\n').unwrap();
+        });
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -61,30 +75,23 @@ impl Dir {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Pos {
-    row: usize,
-    col: usize,
+    idx: usize,
 }
 
 impl Pos {
-    fn step(self, dir: Dir, maze: &[Vec<u8>]) -> Option<Pos> {
-        let offset = match dir {
-            Dir::Up => (-1, 0),
-            Dir::Down => (1, 0),
-            Dir::Left => (0, -1),
-            Dir::Right => (0, 1),
-        };
-
-        // check for integer over-/underflow
-        let pos = Pos {
-            row: self.row.checked_add_signed(offset.0)?,
-            col: self.col.checked_add_signed(offset.1)?,
-        };
-
-        // check that we don't move out of the maze bounds
-        if pos.row >= maze.len() || pos.col >= maze[pos.row].len() {
-            None
-        } else {
-            Some(pos)
+    fn step(self, dir: Dir, maze: &Maze) -> Option<Pos> {
+        match dir {
+            Dir::Up if self.idx >= maze.width => Some(Pos {
+                idx: self.idx - maze.width,
+            }),
+            Dir::Down if self.idx < maze.data.len() - maze.width => Some(Pos {
+                idx: self.idx + maze.width,
+            }),
+            Dir::Left if self.idx % maze.width != 0 => Some(Pos { idx: self.idx - 1 }),
+            Dir::Right if self.idx % maze.width != maze.width - 1 => {
+                Some(Pos { idx: self.idx + 1 })
+            }
+            _ => None,
         }
     }
 }
@@ -96,59 +103,34 @@ struct State {
 }
 
 impl State {
-    fn new(row: usize, col: usize, dir: Dir) -> State {
+    fn new(idx: usize, dir: Dir) -> State {
         State {
-            pos: Pos { row, col },
+            pos: Pos { idx },
             dir,
         }
     }
-    fn step(self, maze: &[Vec<u8>]) -> Option<Self> {
+    fn step(self, maze: &Maze) -> Option<Self> {
         // Move a step in the current direction
         let pos = self.pos.step(self.dir, maze)?;
         // Change direction according to next pipe
-        let dir = self.dir.next(maze[pos.row][pos.col])?;
+        let dir = self.dir.next(maze.data[pos.idx])?;
 
         Some(State { pos, dir })
     }
-    fn mark_step(self, maze: &[Vec<u8>], inner_fields: &mut Vec<Pos>) -> Option<Self> {
-        // dir is pointing in the direction of the exit of the current pipe
-        let insides: SmallVec<[Dir; 2]> = match (maze[self.pos.row][self.pos.col], self.dir) {
-            (b'-', Dir::Left) => smallvec![Dir::Up],
-            (b'-', Dir::Right) => smallvec![Dir::Down],
-            (b'|', Dir::Up) => smallvec![Dir::Right],
-            (b'|', Dir::Down) => smallvec![Dir::Left],
-            (b'7', Dir::Left) => smallvec![Dir::Up, Dir::Right],
-            (b'7', Dir::Down) => smallvec![],
-            (b'F', Dir::Right) => smallvec![],
-            (b'F', Dir::Down) => smallvec![Dir::Up, Dir::Left],
-            (b'J', Dir::Left) => smallvec![],
-            (b'J', Dir::Up) => smallvec![Dir::Right, Dir::Down],
-            (b'L', Dir::Right) => smallvec![Dir::Left, Dir::Down],
-            (b'L', Dir::Up) => smallvec![],
-            _ => smallvec![],
-        };
-
-        insides.iter().for_each(|&dir| {
-            let Some(pos) = self.pos.step(dir, maze) else {
-                return;
-            };
-            // add to the list of inner fields
-            if maze[pos.row][pos.col] == b'.' {
-                if let Err(idx) = inner_fields.binary_search(&pos) {
-                    inner_fields.insert(idx, pos);
-                }
-            }
-        });
-
-        self.step(maze)
-    }
 }
 
-fn start_states(pos: Pos, maze: &[Vec<u8>]) -> [State; 2] {
+fn start_states(maze: &Maze) -> [State; 2] {
+    let start = maze
+        .data
+        .iter()
+        .position(|&b| b == b'S')
+        .map(|idx| Pos { idx })
+        .unwrap();
+
     [Dir::Up, Dir::Down, Dir::Left, Dir::Right]
         .into_iter()
         .filter_map(|dir| {
-            let state = State::new(pos.row, pos.col, dir);
+            let state = State::new(start.idx, dir);
             let _ = state.step(maze)?;
             Some(state)
         })
@@ -157,22 +139,35 @@ fn start_states(pos: Pos, maze: &[Vec<u8>]) -> [State; 2] {
         .unwrap()
 }
 
-fn horizontal_fill(pos: Pos, maze: &mut [Vec<u8>]) {
-    let line = maze[pos.row].as_mut_slice();
+fn fill_inner_fields_row(maze_row: &mut [u8]) {
+    // 'F-7' => false
+    // 'L-J' => false
+    // 'F-J' => true
+    // 'L-7' => true
+    // '|' => true
 
-    for col in (pos.col..line.len()).skip(1) {
-        if line[col] == b'.' {
-            line[col] = b'I';
-        } else {
-            break;
+    let mut prev = None;
+    let mut inside = false;
+
+    for b in maze_row {
+        if *b == b'.' && inside {
+            // mark the spot as inside
+            *b = b'I';
+        } else if *b == b'-' {
+            // this piece is ignored
+            continue;
+        } else if *b == b'|'
+            || *b == b'S' // TODO: This is wrong and wont work for `F-S-7`
+            || prev == Some(b'F') && *b == b'J'
+            || prev == Some(b'L') && *b == b'7'
+        {
+            // we crossed a border
+            inside = !inside;
+            prev = None;
         }
-    }
 
-    for col in (0..pos.col).rev().skip(1) {
-        if line[col] == b'.' {
-            line[col] = b'I';
-        } else {
-            break;
+        if b"7FJL".contains(b) {
+            prev = Some(*b)
         }
     }
 }
@@ -180,24 +175,17 @@ fn horizontal_fill(pos: Pos, maze: &mut [Vec<u8>]) {
 fn main() {
     let mut challenge = advent_of_code_2023::Challenge::start(10, 2);
 
-    let mut maze = challenge
-        .input_lines()
-        .map(|line| line.as_bytes().to_vec())
-        .collect::<Vec<_>>();
+    let mut maze = {
+        let width = challenge.input_lines().next().unwrap().len();
+        let mut data = Vec::new();
+        challenge
+            .input_lines()
+            .for_each(|line| data.extend_from_slice(line.as_bytes()));
+        Maze { data, width }
+    };
     challenge.finish_parsing();
 
-    let start = maze
-        .iter()
-        .enumerate()
-        .find_map(|(row, line)| {
-            line.iter()
-                .position(|&b| b == b'S')
-                .map(|col| Pos { row, col })
-        })
-        .unwrap();
-
-    let starts = start_states(start, &maze);
-
+    let starts = start_states(&maze);
     let finish = starts[1].step(&maze).unwrap();
     let mut current = starts[0].step(&maze).unwrap();
 
@@ -205,41 +193,24 @@ fn main() {
     let mut path = vec![starts[0].pos, current.pos];
     path.sort_unstable();
 
+    // walk the path
     while current.pos != finish.pos {
         current = current.step(&maze).unwrap();
         path.insert(path.binary_search(&current.pos).unwrap_err(), current.pos);
     }
 
     // replace all pipes that are not part of the main loop with ground
-    maze.iter_mut().enumerate().for_each(|(row, line)| {
-        line.iter_mut().enumerate().for_each(|(col, c)| {
-            if path.binary_search(&Pos { row, col }).is_err() {
-                *c = b'.';
-            }
-        })
-    });
+    maze.data
+        .iter_mut()
+        .enumerate()
+        .filter(|&(idx, _)| path.binary_search(&Pos { idx }).is_err())
+        .for_each(|(_, b)| *b = b'.');
 
-    let starts = start_states(start, &maze);
-    let finish = starts[1].step(&maze).unwrap();
+    // mark all inner fields
+    (0..maze.height()).for_each(|row_idx| fill_inner_fields_row(maze.row_mut(row_idx)));
 
-    let mut inner_fields = Vec::new();
-    let mut current = starts[0].mark_step(&maze, &mut inner_fields).unwrap();
-
-    while current.pos != finish.pos {
-        current = current.mark_step(&maze, &mut inner_fields).unwrap();
-    }
-
-    inner_fields.iter().for_each(|&pos| {
-        maze[pos.row][pos.col] = b'I';
-    });
-    inner_fields.iter().for_each(|&pos| {
-        horizontal_fill(pos, &mut maze);
-    });
-
-    let solution = maze
-        .iter()
-        .map(|line| line.iter().filter(|&&c| c == b'I').count())
-        .sum::<usize>();
+    // count the inner fields
+    let solution = maze.data.iter().filter(|&&b| b == b'I').count();
 
     challenge.finish(solution);
 }
